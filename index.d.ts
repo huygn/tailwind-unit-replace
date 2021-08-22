2adb8233fc249446694a9cd2bbe1cb65abac5e73
Huy Giang <gnhuy91@gmail.com>
2021-08-22 10:40:12 +0700

    add func types

--- index.d.ts
+++ index.d.ts
@@ -4,3 +4,9 @@ export type Options = {
   exclude?: (keyof TailwindConfig["theme"])[];
   replacer: (v: string) => string;
 };
+
+export function replaceTalwindUnit(
+  opts: Options
+): (cfg: TailwindConfig) => TailwindConfig;
+
+export function toREM(value: string): string;
